47603fa55d9d2d004c02c4948091abd064d621cd
Yahya Malik <yahyamalik.ofc@gmail.com>
2025-08-24 20:43:55 -0400

    fix: simplify Auth component; link to signin/signup

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -7,15 +7,8 @@ export default function Auth() {
     <div className="max-w-md mx-auto p-6">
       <h1 className="text-2xl font-semibold mb-4">Welcome to EmailPro</h1>
       <p className="mb-6">
-        Please{' '}
-        <Link href="/auth/signin" className="underline">
-          sign in
-        </Link>{' '}
-        or{' '}
-        <Link href="/auth/signup" className="underline">
-          create an account
-        </Link>
-        .
+        Please <Link href="/auth/signin" className="underline">sign in</Link> or{' '}
+        <Link href="/auth/signup" className="underline">create an account</Link>.
       </p>
     </div>
   );
